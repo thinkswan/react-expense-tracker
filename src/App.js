2f284c77a0095e352d0a14c7213f386c4c0f257e
Graham Swan <thinkswan@gmail.com>
2020-02-22 21:48:23 +0100

    Add context, reducer, and provider

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Balance } from "./components/Balance"
 import { IncomeExpenses } from "./components/IncomeExpenses"
 import { TransactionList } from "./components/TransactionList"
 import { AddTransaction } from "./components/AddTransaction"
+import { GlobalProvider } from "./context/GlobalState"
 import "./App.css"
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
 
       <div className="container">
@@ -17,7 +18,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   )
 }
 
