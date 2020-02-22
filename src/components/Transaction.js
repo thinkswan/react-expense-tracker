229050ef9a20b1c616ad1346978800954f44ccb9
Graham Swan <thinkswan@gmail.com>
2020-02-22 21:55:34 +0100

    Format list items

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,15 @@
 import React from "react"
 
 export const Transaction = ({ transaction }) => {
+  const itemClass = transaction.amount < 0 ? "minus" : "plus"
+  const sign = transaction.amount < 0 ? "-" : "+"
+
   return (
-    <li className="minus">
-      {transaction.text} <span>-$400</span>
+    <li className={itemClass}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   )
